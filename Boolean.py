fc0a5f13d4e9a47d5709e77d461b5b9334a7f94e
asmit404 <asmittbhardwaj@gmail.com>
2023-09-18 21:02:43 +0530

    Create Boolean.py

---
+++ Boolean.py
@@ -0,0 +1,13 @@
+"""
+Title       : Boolean
+Link        : https://dmoj.ca/problem/boolean
+Author      : Asmit Singh
+Solved On   : 18 Sept 2023
+Solved Using    : Python3
+"""
+
+a = input()
+print(eval(a))
+
+# Time Complexity  : O(1)
+# Space Complexity : O(1)
