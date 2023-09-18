5cffaa437613103791e5116c502722a7289085ea
asmit404 <asmittbhardwaj@gmail.com>
2023-09-18 21:07:35 +0530

    Create A plus B.py

---
+++ A plus B.py
@@ -0,0 +1,14 @@
+"""
+Title       : Boolean
+Link        : https://dmoj.ca/problem/aplusb
+Author      : Asmit Singh
+Solved On   : 18 Sept 2023
+Solved Using    : Python3
+"""
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    print(a + b)
+
+# Time Complexity  : O(n)
+# Space Complexity : O(1)
