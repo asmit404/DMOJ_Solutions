743d440a6441651b5f3103660980e86171550baa
asmit404 <asmittbhardwaj@gmail.com>
2023-09-18 20:22:21 +0530

    Create List Minimum.py

---
+++ List Minimum.py
@@ -0,0 +1,13 @@
+"""
+Title       : List Minimum
+Link        : https://dmoj.ca/problem/bf1
+Author      : Asmit Singh
+Solved On   : 18 Sept 2023
+Solved Using    : Python3
+"""
+
+pool = sorted([int(input()) for _ in range(int(input()))])
+print(*pool, sep='\n')
+
+# Time Complexity  : O(nlogn)
+# Space Complexity : O(n)
